b68c12637a857ca90008c08f66dace30114872ea
BernieTomson <bernietomson@gmail.com>
2025-03-22 13:35:40 +0100

    commit 2

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly. It really works!!
+            Save and see your changes instantly. It really works like a clock...
           </li>
         </ol>
 
